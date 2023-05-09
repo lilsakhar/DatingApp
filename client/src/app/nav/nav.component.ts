a9d28b7540ea483d7729cea40b6454cc08a55fef
lsakharova <l.sakharova@dtpoland.com>
2023-05-09 08:40:54 +0200

    before Angular update

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { AccounService } from '../_services/accoun.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  model: any = {}
+  loggedIn = false;
+
+  constructor(private accountService: AccounService) { }
+
+  ngOnInit(): void {
+  }
+
+  login() {
+    this.accountService.login(this.model).subscribe({
+      next: response => {
+        console.log(response);
+        this.loggedIn = true;
+      },
+      error: error => console.log(error)
+    })
+  }
+
+  logout() {
+    this.loggedIn = false;
+  }
+
+}
